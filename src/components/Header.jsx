b73c30861f06f952351ca6b9177f4fafb8ee3af9
NSri4112005 <nithyasriparivallal@gmail.com>
2026-08-30 10:22:00 +0530

    Update header and styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 function Header() {
   return (
     <header className="header">
-      <h1>Product Store</h1>
+      <h1>PRODUCT STORE</h1>
       <p>Find your favorite products</p>
     </header>
   );
